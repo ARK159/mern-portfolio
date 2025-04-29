6e31fad8fe039978f49ea55857211885138042b2
ARJUN RAY <89641876+ARK159@users.noreply.github.com>
2025-04-29 14:25:37 +0545

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname,'./client/build')))
 //routes
 app.use('/api/v1/portfolio',require('./routes/portfolioRoutes'))
-app.get('*',function(req,res){
+app.get('/',function(req,res){
     res.sendFile(path.join(__dirname,'./client/build/index.html'))
 })
 //port
